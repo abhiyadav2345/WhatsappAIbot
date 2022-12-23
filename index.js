9d355f8bf3f320a8184670a4e514d755fc02b3f7
Abhishek Yadav <34111525+abhiyadav2345@users.noreply.github.com>
2022-12-23 12:42:28 +0530

    error resolved

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-const express = require("express");
-const homeController = require("../controllers/home_controller");
+express = require('express')
+const homeController = require('./controllers/home_controller')
 
-const app = express();
-const port = 8000;
-app.use(express.static("./assets"));
 
-// set up the view engine
-app.set("view engine", "ejs");
-app.set("views", "./views");
 
-//
 
-console.log("Router Loaded");
-app.get("/", homeController.home);
+const app = express()
+const port = 8000
+app.use(express.static('./assets'))
 
-module.exports = router;
+app.get('/', homeController.home)
 
 app.listen(port, function (err) {
   if (err) {
     // console.log("Error:", err);
-    console.log(`Error in running the server :",$(err)`);
+    console.log(`Error in running the server :",$(err)`)
   }
-  console.log(`server is running on port :${port}`);
-});
+  console.log(`server is running on port :${port}`)
+})
